e8df5cbbaae1bd4218811d29bf004da9d80cdbed
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-03-15 11:23:14 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let y = 0
+let x = 0
+joystickbit.initJoystickBit()
+radio.setGroup(1)
+basic.showIcon(IconNames.Yes)
+basic.showString("T")
+basic.forever(function () {
+    x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
+    y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 1023, 0, 100, -100)
+    radio.sendValue("x", x)
+    radio.sendValue("y", y)
+})
