dba2773731090995e63f66ff9eec2b3aed9b6ddd
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-04-30 07:47:54 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,4 +9,16 @@ basic.forever(function () {
     y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 1023, 0, 100, -100)
     radio.sendValue("x", x)
     radio.sendValue("y", y)
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        radio.sendString("C")
+    }
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendString("D")
+    }
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
+        radio.sendString("E")
+    }
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P15)) {
+        radio.sendString("F")
+    }
 })
